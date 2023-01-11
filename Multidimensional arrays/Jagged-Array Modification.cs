b159a5c9c490cde710db25b280d8853a25529975
Slavi Bozhilov <94463901+SlaviB02@users.noreply.github.com>
2023-01-11 23:44:09 +0200

    Add files via upload

---
+++ Multidimensional arrays/Jagged-Array Modification.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowSize = int.Parse(Console.ReadLine());
+            int[][] matrix = new int[rowSize][];
+            for(int i=0;i<rowSize;i++)
+            {
+                int[] columns = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                matrix[i] = columns;
+            }
+            while(true)
+            {
+                string[] command = Console.ReadLine().Split();
+                if(command[0]=="END")
+                {
+                  
+                    break;
+                }
+                    int row = int.Parse(command[1]);
+                    int column = int.Parse(command[2]);
+                int value = int.Parse(command[3]);
+                if(row<0 || row>=matrix.Length || column<0 ||column>=matrix[row].Length)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+                else
+                {
+                    if(command[0]=="Add")
+                    {
+                        matrix[row][column] += value;
+                    }
+                    else
+                    {
+                        matrix[row][column] -= value;
+                    }
+                }
+            }
+            foreach(int[]element in matrix)
+            {
+                Console.WriteLine(string.Join(" ",element));
+            }
+
+        }
+    }
+}
---
+++ Multidimensional arrays/Square with Max Sum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = input[0];
+            int columns = input[1];
+            int[,] matrix = new int[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = numbers[j];
+                }
+            }
+            int bestSum = 0;
+            int bestRow = 0;
+            int bestColumn = 0;
+            for (int i = 0; i < rows-1; i++)
+            {
+               
+                for (int j = 0; j < columns-1; j++)
+                {
+                    var newSum = matrix[i, j] + matrix[i + 1, j] + matrix[i, j + 1] + matrix[i + 1, j + 1];
+                    if(newSum>bestSum)
+                    {
+                        bestSum = newSum;
+                        bestRow = i;
+                        bestColumn = j;
+                    }
+                }
+            }
+            Console.WriteLine(matrix[bestRow, bestColumn] + " " + matrix[bestRow , bestColumn+1]);
+            Console.WriteLine(matrix[bestRow+1, bestColumn] + " " + matrix[bestRow + 1, bestColumn+1]);
+            Console.WriteLine(bestSum);
+
+
+        }
+    }
+}
